f4177b9d0b19454760f6f6424aed1b26dfa97a71
Tomáš Vaisar <incik@daedalus.local>
2015-04-09 01:06:54 +0200

    "Fill in" step

--- features/step_definitions/global_steps.rb
+++ features/step_definitions/global_steps.rb
@@ -3,13 +3,19 @@ When(/^I go to homepage$/) do
 end
 
 Given /^I am logged in$/ do
+  FactoryGirl.create :tomasvaisarcz
+
   delete destroy_user_session_path
   visit new_user_session_path
-  fill_in 'email', :with => 'tomas@vaisar.cz'
-  fill_in 'password', :with => 'heslo'
+  fill_in 'user_email', :with => 'tomas@vaisar.cz'
+  fill_in 'user_password', :with => 'heslo'
   click_button('save')
 end
 
 Given /^I am not logged in$/ do
   delete destroy_user_session_path
+end
+
+When(/^I fill "(.*?)" with "(.*?)"$/) do |element, value|
+  fill_in(element, value)
 end
