a7212c38deeba9deb86d5a0f99b5c7c2aa3fd872
Tomáš Vaisar <incik@daedalus.local>
2015-04-09 14:44:46 +0200

    Satisfying dashboard tests

--- features/step_definitions/dashboard_steps.rb
+++ features/step_definitions/dashboard_steps.rb
@@ -4,3 +4,15 @@ Given(/^I have at least one invoice$/) do
   visit '/'
   expect(page).to have_css('.invoice-row', count: 1)
 end
+
+When(/^I expand invoice$/) do
+  first('.invoice-expander').click
+end
+
+Then(/^I should see invoice lines$/) do
+  expect(page).to have_css('.invoice-detail-row')
+end
+
+Then(/^I should see edit link$/) do
+  pending # express the regexp above with the code you wish you had
+end
