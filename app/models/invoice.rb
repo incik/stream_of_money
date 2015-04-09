e71906924a7f0b10d74100a44ba9e8b237afca40
Tomáš Vaisar <incik@daedalus.local>
2015-04-09 17:04:34 +0200

    Invoice counts

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -11,4 +11,13 @@ class Invoice < ActiveResource::Base
   def expenses
     Expense.where(invoice_id: self.id)
   end
+
+  def expenses_cost
+    expenses.inject(0) { |sum, expense| sum + expense.expense }
+  end
+
+  def result_cost
+    total_amount - expenses_cost
+  end
+
 end
--- spec/factories/expenses.rb
+++ spec/factories/expenses.rb
@@ -1,7 +1,9 @@
 FactoryGirl.define do
 
   factory :expense do
-
+    invoice_id 99941
+    line_index 0
+    expense 10000
   end
 
 end
