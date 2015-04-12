c5dcb1cac49dd4938850edc5d6dfb4e331f19ed6
Tomáš Vaisar <incik@daedalus.local>
2015-04-12 23:10:14 +0200

    Expense controller basics

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,11 +3,29 @@ class ExpensesController < ApplicationController
   include ApiCommunicator
 
   def create
-
+    @expense = Expense.new(expense_params)
+    if @expense.save
+      flash[:success] = 'Saved!'
+    else
+      flash[:error] = 'Error while saving expense'
+    end
+    redirect_to root_path
   end
 
   def update
-
+    @expense = Expense.find(params[:id])
+    if @expense.update_attributes(expense_params)
+      flash[:success] = 'Saved!'
+    else
+      flash[:error] = 'Error while saving expense'
+    end
+    redirect_to root_path
   end
+
+  private
+
+    def expense_params
+      params.require(:expense).permit(:id, :invoice_id, :description, :expense)
+    end
 
 end
