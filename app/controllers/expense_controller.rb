ed6a8895166e9a8dd0bead6ea485226f83cb6738
Tomáš Vaisar <incik@daedalus.local>
2015-04-12 21:38:36 +0200

    First edit form

---
+++ app/controllers/expense_controller.rb
@@ -0,0 +1,11 @@
+class ExpenseController < ApplicationController
+  include AuthController
+  include ApiCommunicator
+
+  def create
+  end
+
+  def update
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { 'registrations': 'registration'}
   root to: 'dashboard#index'
 
-  resources :invoices
+  resources :invoices, only: [:index, :edit]
+  resources :expenses, only: [:create, :update, :destroy]
 end
