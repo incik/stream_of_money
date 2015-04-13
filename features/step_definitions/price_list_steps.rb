1311b114dcdb8d83e5198d64047477a2afadf0e1
Tomáš Vaisar <incik@daedalus.local>
2015-04-13 08:45:15 +0200

    Adding price list tests as pending

---
+++ features/step_definitions/price_list_steps.rb
@@ -0,0 +1,35 @@
+Given(/^I go to price list$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I select "(.*?)" for price list item$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see "(.*?)" equal to "(.*?)"$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I see no price list items$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I go to add price list item$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I see new price list item form$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I save price list item$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see one price list item$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see error$/) do
+  pending # express the regexp above with the code you wish you had
+end
